a100fdcf378d0a454cebfaab57e0407d656d25aa
LatinLady <marielblyden@hotmail.com>
2022-12-29 16:10:42 -0400

    finish modification

--- script.js
+++ script.js
@@ -1,59 +1,77 @@
-var generate = document.querySelector("#generate");
+// Variables declaration
+//lowercase character
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "m", "n", "o", "p", "q", "r", "s", "t", "v", "x", "y", "z"];
+//uppercase character
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "T", "V", "W", "X", "Y", "Z"];
+//numbers character
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+//special character
+var specialChars = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "/", "~"];
 
-var lowercase = "abcdefghijklmnopqrstvwxyz";
-var uppercase = "ABCDEFGHIJKLMNOPQRSTVWXYZ";
-var numbers = "1234567890";
-var specialChar = "!@#$%^&*()_+=][~";
+var enter;
+
+var confirmLowercase;
+var confirmUppercase;
+var confirmNumbers;
+var confirmSpecialChars;
 
 function generatePassword(){
-    var password = "";
-    var passwordChars = "";
-// Prompt message
-    var passwordLenght = prompt("How many character you want your password? Password must to br between 8-128 characters.");
-    passwordLenght = parseInt(passwordLenght);
-    if (passwordLenght < 8){
-        alert("Password is to short.");
-    }
+    var generateBtn = document.querySelector("#generate");
+    var password = prompt("Choose 8 and 128");
 
-    if (passwordLenght > 128){
-        alert("Password is to long.");
-    }
-
-// Create Booleans Yes or No
-var lowercaseChoice = confirm("Choose a lowercase letter.");
-
-if (lowercaseChoice){
-    passwordChars += lowercase;
+    return password;
 }
 
-var uppercaseChoise = confirm("Choose a uppercase letter.");
+if (!enter){
+    alert("Enter an password");
+} else if (enter < 8 || enter > 128){
+    enter = parseInt(prompt("Choose between 8 and 128"));
+} else {
 
-if (uppercaseChoise){
-    passwordChars += uppercase;
+    var confirmLowercase = confirm("Include a lowercase");
+    var confirmUppercase = confirm("Include a uppercase");
+    var confirmNumbers = confirm("Include a number");
+    var confirmSpecialChars = confirm("Include a specialchars");
+};
+
+if (!confirmLowercase && !confirmUppercase && !confirmNumbers && !confirmSpecialChars) {
+    alert("You must to choice at least one parameter");
+};
+
+var confirmLowercase = confirm("Include a lowercase");
+var confirmUppercase = confirm("Include a uppercase");
+var confirmNumbers = confirm("Include a number");
+var confirmSpecialChars = confirm("Include a specialchars");
+
+if (confirmLowercase) {
+    passwordChars = passwordChars.concat(lowercase);
 }
-var numbersChoice = confirm("Choose a numbers.");
 
-if (numbersChoice){
-    passwordChars += number;
+else if (confirmUppercase) {
+    passwordChars = passwordChars.concat(uppercase);
 }
 
-var specialCharChoice = confirm("Choose a specialchar letter.");
+else if (confirmNumbers) {
+    passwordChars = passwordChars.concat(Numbers);
 
-if (specialCharChoice){
-    passwordChars += specialChar;
 }
 
-// The for loop for get the password
-for (var i = 0; i < passwordLenght; i++){
-    password = passwordChars[Math.floor(Math.random() * passwordChars.length)]
+else if (confirmSpecialChars) {
+    passwordChars = passwordChars.concat(specialChars);
+
+};
+
+var passwordChars = [];
+
+for (var i = 0; i < enter; i++){
+    var randomPassword = randomPassword[Math.floor(Math.random() * randomPassword.length)];
 }
 
-function input(){
-    var password = generatePassword();
-    var password = document.querySelector("#password");
-    password.value = password;
-}
+function writePassword(){
+    var password = generateBtn();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
 
 generate.addEventListener("click",() => {password.value = generatePassword();
-});   
+});
